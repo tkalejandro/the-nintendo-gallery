ad9b7aa1e029010a8353f316c7a311dfcb648543
Alejandro <tk.jalejandro@gmail.com>
2021-08-04 16:20:55 +0200

    small correction to objects information

--- js/module/00_01_metroidImages.js
+++ js/module/00_01_metroidImages.js
@@ -2,63 +2,75 @@ export const metroidImages = [
     {
         author: "Артем Бурлик",
         imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/disp/72a319113606963.602b97b532122.jpg",
-        referenceURL: "https://www.behance.net/gallery/113606963/Super-Metroid-Samus"
+        referenceURL: "https://www.behance.net/gallery/113606963/Super-Metroid-Samus",
+        franchise: "metroid"
     },
     {
         author: "Jerrison Grimminck",
         imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/569c1d110765437.5ff511648d79e.png",
-        referenceURL: "https://www.behance.net/gallery/110765437/Gravity-Suit"
+        referenceURL: "https://www.behance.net/gallery/110765437/Gravity-Suit",
+        franchise: "metroid"
     },
     {
         author: "Illustrata Studio",
         imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/933be583575961.5efb45a1bb46f.jpg",
-        referenceURL: "https://www.behance.net/gallery/83575961/Inside-the-heroes-head"
+        referenceURL: "https://www.behance.net/gallery/83575961/Inside-the-heroes-head",
+        franchise: "metroid"
     },
     {
         author: "Erik Vicino",
         imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/20952b103863215.5f5691be83db1.jpg",
-        referenceURL: "https://www.behance.net/gallery/103863215/Metroid-Photoshoot-Mission"
+        referenceURL: "https://www.behance.net/gallery/103863215/Metroid-Photoshoot-Mission",
+        franchise: "metroid"
     },
     {
         author: "Oh Boy A Toy!(He/Him)",
         imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/747ed3110685589.5ff39a4c04021.jpg",
-        referenceURL: "https://www.behance.net/gallery/110685589/Samus-and-Metroid"
+        referenceURL: "https://www.behance.net/gallery/110685589/Samus-and-Metroid",
+        franchise: "metroid"
     },
     {
         author: "Daniel Travieso",
         imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/fe7617115507409.604ff3919a90e.jpg",
-        referenceURL: "https://www.behance.net/gallery/115507409/Samus-Aran-Metroid-Hunter"
+        referenceURL: "https://www.behance.net/gallery/115507409/Samus-Aran-Metroid-Hunter",
+        franchise: "metroid"
     },
     {
         author: "Esteban Barrientos",
         imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/e0eb17117431531.6075bfce33c46.jpg",
-        referenceURL: "https://www.behance.net/gallery/117431531/Samus"
+        referenceURL: "https://www.behance.net/gallery/117431531/Samus",
+        franchise: "metroid"
     },
     {
         author: "Nico Vliek",
         imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/disp/e875d112430987.5626913fb605b.jpg",
-        referenceURL: "https://www.behance.net/gallery/12430987/Metroid-Fusion-Poster"
+        referenceURL: "https://www.behance.net/gallery/12430987/Metroid-Fusion-Poster",
+        franchise: "metroid"
     },
     {
         author: "Danilu Molina",
         imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/1e7fe7123046717.60e62a47ad7ad.jpg",
-        referenceURL: "https://www.behance.net/gallery/123046717/Metroid"
+        referenceURL: "https://www.behance.net/gallery/123046717/Metroid",
+        franchise: "metroid"
     },
     {
         author: "Anaiis Debusschere",
         imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/1ab690111792877.6008706d3e243.jpg",
-        referenceURL: "https://www.behance.net/gallery/111792877/Metroid-Revenge"
+        referenceURL: "https://www.behance.net/gallery/111792877/Metroid-Revenge",
+        franchise: "metroid"
     },
     {
         author: "Anaiis Debusschere",
         imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/73c2d7111792877.6008706d3efe4.jpg",
-        referenceURL: "https://www.behance.net/gallery/111792877/Metroid-Revenge"
+        referenceURL: "https://www.behance.net/gallery/111792877/Metroid-Revenge",
+        franchise: "metroid"
     },
     {
         author: "Anaiis Debusschere",
         imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/0e8bd8111792877.6008706d3e9a1.jpg",
-        referenceURL: "https://www.behance.net/gallery/111792877/Metroid-Revenge"
-    },
+        referenceURL: "https://www.behance.net/gallery/111792877/Metroid-Revenge",
+        franchise: "metroid"
+    }
 
 ]
 
--- js/module/00_02_pokemonImages.js
+++ js/module/00_02_pokemonImages.js
@@ -2,56 +2,67 @@ export const pokemonImages = [
     {
         author: "Multiple Owners",
         imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/41f2ae115606005.6051ca43c30bd.jpg",
-        referenceURL: "https://www.behance.net/gallery/115606005/Iconic-Pokmon-V1?tracking_source=search_projects_recommended%7Cpokemon"
+        referenceURL: "https://www.behance.net/gallery/115606005/Iconic-Pokmon-V1",
+        franchise: "pokemon"
     },
     {
         author: "Multiple Owners",
         imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/1a6cbe115606005.6051ca42c02f9.jpg",
-        referenceURL: "https://www.behance.net/gallery/115606005/Iconic-Pokmon-V1?tracking_source=search_projects_recommended%7Cpokemon"
+        referenceURL: "https://www.behance.net/gallery/115606005/Iconic-Pokmon-V1",
+        franchise: "pokemon"
     },
     {
         author: "Multiple Owners",
         imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/2e84f1115606005.6051ca43c2b53.jpg",
-        referenceURL: "https://www.behance.net/gallery/115606005/Iconic-Pokmon-V1?tracking_source=search_projects_recommended%7Cpokemon"
+        referenceURL: "https://www.behance.net/gallery/115606005/Iconic-Pokmon-V1",
+        franchise: "pokemon"
     },
     {
         author: "KFC China",
         imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/485ae8112549033.6016d83fc5e84.jpg",
-        referenceURL: "https://www.behance.net/gallery/112549033/KFCCHINA2021-POKEMON-CHINESE-NEW-YEAR?tracking_source=search_projects_recommended%7Cpokemon"
+        referenceURL: "https://www.behance.net/gallery/112549033/KFCCHINA2021-POKEMON-CHINESE-NEW-YEAR",
+        franchise: "pokemon"
     },
     {
         author: "KFC China",
         imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/f7a9c3112549033.6016d83fc3ec8.jpg",
-        referenceURL: "https://www.behance.net/gallery/112549033/KFCCHINA2021-POKEMON-CHINESE-NEW-YEAR?tracking_source=search_projects_recommended%7Cpokemon"
+        referenceURL: "https://www.behance.net/gallery/112549033/KFCCHINA2021-POKEMON-CHINESE-NEW-YEAR",
+        franchise: "pokemon"
     },
     {
         author: "KFC China",
         imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/b1764c112549033.6016d83fc7ab0.jpg",
-        referenceURL: "https://www.behance.net/gallery/112549033/KFCCHINA2021-POKEMON-CHINESE-NEW-YEAR?tracking_source=search_projects_recommended%7Cpokemon"
+        referenceURL: "https://www.behance.net/gallery/112549033/KFCCHINA2021-POKEMON-CHINESE-NEW-YEAR",
+        franchise: "pokemon"
     },
     {
         author: "KFC China",
         imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/4b5f35112549033.6016d83fc49e0.jpg",
-        referenceURL: "https://www.behance.net/gallery/112549033/KFCCHINA2021-POKEMON-CHINESE-NEW-YEAR?tracking_source=search_projects_recommended%7Cpokemon"
+        referenceURL: "https://www.behance.net/gallery/112549033/KFCCHINA2021-POKEMON-CHINESE-NEW-YEAR",
+        franchise: "pokemon"
     },
     {
         author: "Multiple Owners",
         imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/bc973f91313719.5e2efec43b72e.png",
-        referenceURL: "https://www.behance.net/gallery/91313719/Eevee-Blender?tracking_source=search_projects_recommended%7Ceevee%20pokemon"
+        referenceURL: "https://www.behance.net/gallery/91313719/Eevee-Blender",
+        franchise: "pokemon"
     },
     {
         author: "Mar Rodriguez",
         imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/a457eb78566667.5ca88b3f1f6e4.png",
-        referenceURL: "https://www.behance.net/gallery/78566667/Pokemon-Sword-and-Shield-fanart?tracking_source=search_projects_recommended%7Ceevee%20pokemon"
+        referenceURL: "https://www.behance.net/gallery/78566667/Pokemon-Sword-and-Shield-fanart",
+        franchise: "pokemon"
     },
     {
         author: "Mar Rodriguez",
         imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/b432a678566667.5ca88b3f1fc1d.png",
-        referenceURL: "https://www.behance.net/gallery/78566667/Pokemon-Sword-and-Shield-fanart?tracking_source=search_projects_recommended%7Ceevee%20pokemon"
+        referenceURL: "https://www.behance.net/gallery/78566667/Pokemon-Sword-and-Shield-fanart",
+        franchise: "pokemon"
     },
     {
         author: "Mar Rodriguez",
         imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/03170378566667.5ca88b3f1ff9a.png",
-        referenceURL: "https://www.behance.net/gallery/78566667/Pokemon-Sword-and-Shield-fanart?tracking_source=search_projects_recommended%7Ceevee%20pokemon"
-    },
+        referenceURL: "https://www.behance.net/gallery/78566667/Pokemon-Sword-and-Shield-fanart",
+        franchise: "pokemon"
+    }
 ]
