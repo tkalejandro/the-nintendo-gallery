ff11a5c05c509d36e5e4dec4395bf98a41a9c3c8
Alejandro <tk.jalejandro@gmail.com>
2021-08-04 15:48:33 +0200

    Metroid images object and Pokemon images object created

---
+++ js/module/00_01_metroidImages.js
@@ -0,0 +1,64 @@
+export const metroidImages = [
+    {
+        author: "Артем Бурлик",
+        imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/disp/72a319113606963.602b97b532122.jpg",
+        referenceURL: "https://www.behance.net/gallery/113606963/Super-Metroid-Samus"
+    },
+    {
+        author: "Jerrison Grimminck",
+        imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/569c1d110765437.5ff511648d79e.png",
+        referenceURL: "https://www.behance.net/gallery/110765437/Gravity-Suit"
+    },
+    {
+        author: "Illustrata Studio",
+        imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/933be583575961.5efb45a1bb46f.jpg",
+        referenceURL: "https://www.behance.net/gallery/83575961/Inside-the-heroes-head"
+    },
+    {
+        author: "Erik Vicino",
+        imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/20952b103863215.5f5691be83db1.jpg",
+        referenceURL: "https://www.behance.net/gallery/103863215/Metroid-Photoshoot-Mission"
+    },
+    {
+        author: "Oh Boy A Toy!(He/Him)",
+        imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/747ed3110685589.5ff39a4c04021.jpg",
+        referenceURL: "https://www.behance.net/gallery/110685589/Samus-and-Metroid"
+    },
+    {
+        author: "Daniel Travieso",
+        imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/fe7617115507409.604ff3919a90e.jpg",
+        referenceURL: "https://www.behance.net/gallery/115507409/Samus-Aran-Metroid-Hunter"
+    },
+    {
+        author: "Esteban Barrientos",
+        imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/e0eb17117431531.6075bfce33c46.jpg",
+        referenceURL: "https://www.behance.net/gallery/117431531/Samus"
+    },
+    {
+        author: "Nico Vliek",
+        imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/disp/e875d112430987.5626913fb605b.jpg",
+        referenceURL: "https://www.behance.net/gallery/12430987/Metroid-Fusion-Poster"
+    },
+    {
+        author: "Danilu Molina",
+        imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/1e7fe7123046717.60e62a47ad7ad.jpg",
+        referenceURL: "https://www.behance.net/gallery/123046717/Metroid"
+    },
+    {
+        author: "Anaiis Debusschere",
+        imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/1ab690111792877.6008706d3e243.jpg",
+        referenceURL: "https://www.behance.net/gallery/111792877/Metroid-Revenge"
+    },
+    {
+        author: "Anaiis Debusschere",
+        imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/73c2d7111792877.6008706d3efe4.jpg",
+        referenceURL: "https://www.behance.net/gallery/111792877/Metroid-Revenge"
+    },
+    {
+        author: "Anaiis Debusschere",
+        imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/0e8bd8111792877.6008706d3e9a1.jpg",
+        referenceURL: "https://www.behance.net/gallery/111792877/Metroid-Revenge"
+    },
+
+]
+
