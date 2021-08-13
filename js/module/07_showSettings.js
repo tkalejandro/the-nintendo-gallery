0822652dfe28b16a63cef753c17a0bb878128ee3
Alejandro <tk.jalejandro@gmail.com>
2021-08-13 09:49:39 +0200

    btn cog created and working

--- js/module/07_showSettings.js
+++ js/module/07_showSettings.js
@@ -1,4 +1,4 @@
-import { innerSettingsContainer } from "./00_variables.js"
+import { innerSettingsContainer, settingTitle } from "./00_variables.js"
 
 
 //toggle help to add and remove the same class, no need to create extra logic
@@ -6,4 +6,8 @@ import { innerSettingsContainer } from "./00_variables.js"
 export const showSettings = () => {
    
    innerSettingsContainer.classList.toggle("d-none")
+   settingTitle.classList.toggle("d-none")
+
+   //Now we when h2 appears we need to make sure is invisible, we want to keep it just for semantic purposes
+   settingTitle.classList.toggle("v-hidden")
 }
