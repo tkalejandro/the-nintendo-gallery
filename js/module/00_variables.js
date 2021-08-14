5cd9f13f5b076ae3c20457e9761b1e3194e76274
Alejandro <tk.jalejandro@gmail.com>
2021-08-14 14:39:33 +0200

    Checkbox All added to html and organize variables to better read

--- js/module/00_variables.js
+++ js/module/00_variables.js
@@ -1,12 +1,26 @@
-export const galleryContainer = document.querySelector(".gallery")
-export const numberColumns = document.querySelector("#numberCol")
-export const numberGallery = document.querySelector("#numberGallery")
+//Intro container
 export const introContainer = document.querySelector("#intro")
+export const startBtn = document.querySelector("#startBtn")
+
+
+//****************** */
+
+//Settings container 
 export const settingsContainer = document.querySelector("#settings")
 export const innerSettingsContainer = document.querySelector(".settings-container")
+export const settingsBtn = document.querySelector("#settingsBtn")
 export const settingTitle = document.querySelector("h2")
+export const numberColumns = document.querySelector("#numberCol")
+export const numberGallery = document.querySelector("#numberGallery")
+export const franchiseBtn = document.querySelector("#franchiseBtn")
+//Franchise container
+export const allGames = document.querySelector("#allGames")
+export const franchiseCheckbox = document.querySelector(".franchiseCheckbox")
 export const metroid = document.querySelector("#metroid")
 export const pokemon = document.querySelector("#pokemon")
 export const zelda = document.querySelector("#zelda")
-export const startBtn = document.querySelector("#startBtn")
-export const settingsBtn = document.querySelector("#settingsBtn")
+
+// ***************** */
+
+//Gallery container
+export const galleryContainer = document.querySelector(".gallery")
