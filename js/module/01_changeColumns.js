09ba2994744982faed16fda36276d24a06792690
Alejandro <tk.jalejandro@gmail.com>
2021-08-15 16:11:39 +0200

    Column change bug fixed. The problem was change column was deleting d-none, now d-none its added in case it had it before.

--- js/module/01_changeColumns.js
+++ js/module/01_changeColumns.js
@@ -10,8 +10,10 @@ export const changeColumns = () => {
     let currentSelect = numberColumns.value
 
     allImages.forEach(image => {
-        //Empty the className before adding a new one. This way we avoid stacking classes.
-        image.className = ""
+        //Empty the className before adding a new one. This way we avoid stacking classes. 
+        //We also need to create some logic, to avoid conflict with the images that has d-none. We want to keep it that way.
+        image.classList.contains("d-none") ? (image.className = "", image.classList.add("d-none")) : image.className = ""
+        
 
         //Now we select the columns to show!
         currentSelect == 1
