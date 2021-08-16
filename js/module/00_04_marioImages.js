2b8636ea268b7df45593cd82e1f8f34ade9929c2
Alejandro <tk.jalejandro@gmail.com>
2021-08-16 08:51:37 +0200

    Mario Images Added

---
+++ js/module/00_04_marioImages.js
@@ -0,0 +1,69 @@
+export const marioImages = [
+    {
+        author: "Multiple Owners",
+        imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/edd0dd90235489.5e123fb89e636.jpg",
+        referenceURL: "https://www.behance.net/gallery/90235489/FanArt-Koopa-Beach-Mario-Kart-64",
+        franchise: "mario"
+    },
+    {
+        author: "lago Brasil",
+        imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/7ae191119240223.60a84de8e0683.png",
+        referenceURL: "https://www.behance.net/gallery/119240223/Super-Mario-64-Fanart-2D-Style",
+        franchise: "mario"
+    },
+    {
+        author: "Gemma Roman",
+        imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/6fec9572782515.5bf3672d0a3b5.jpg",
+        referenceURL: "https://www.behance.net/gallery/72782515/The-Princess-and-the-Plumber-Super-Mario-NY-Artshow",
+        franchise: "mario"
+    },
+
+    {
+        author: "Wesley Ellis",
+        imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/disp/06cc7d21449545.563016e0928f3.jpg",
+        referenceURL: "https://www.behance.net/gallery/21449545/Princess-Peach-Watercolors",
+        franchise: "mario"
+    },
+    {
+        author: "Jose Angelo Felix",
+        imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/52bf1326532121.56356a0c9f216.gif",
+        referenceURL: "https://www.behance.net/gallery/26532121/A-Winner-is-You",
+        franchise: "mario"
+    },
+    {
+        author: "Xel Got",
+        imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/disp/aa78dc26983825.5625d283ef249.jpg",
+        referenceURL: "https://www.behance.net/gallery/26983825/Super-Princess-Peach",
+        franchise: "mario"
+    },
+    {
+        author: "Boom Fish",
+        imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/9d1a5f111947029.600b16976eb18.png",
+        referenceURL: "https://www.behance.net/gallery/111947029/Sponsors-Mario-Kart-8-Toadettes-coffee",
+        franchise: "mario"
+    },
+    {
+        author: "Synyster angel",
+        imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/disp/d51da417341997.562b8c71b22e9.jpg",
+        referenceURL: "https://www.behance.net/gallery/17341997/mario-kART-artjam-",
+        franchise: "mario"
+    },
+    {
+        author: "Paulo Gabriel",
+        imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/385c83107890051.5fb1d07794301.png",
+        referenceURL: "https://www.behance.net/gallery/107890051/Bowsett",
+        franchise: "mario"
+    },
+    {
+        author: "liz michelle prim davila",
+        imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/765ab5115806021.60553713a8ca8.png",
+        referenceURL: "https://www.behance.net/gallery/115806021/Bowser",
+        franchise: "mario"
+    },
+    {
+        author: "Evan Nixon",
+        imageURL: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/028a5e15678551.56295b50c7499.png",
+        referenceURL: "https://www.behance.net/gallery/15678551/Yoshis-Island-Accordion-Book",
+        franchise: "mario"
+    }
+]
